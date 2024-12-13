e23c3c94ceb1ce0832db4d5713108e6874a7134c
vemm8ks2 <vemm8ks2@gmail.com>
2024-12-13 09:24:18 +0900

    CHORE: 프로젝트 셋업

---
+++ src/main/java/com/vemm8ks2/WebSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vemm8ks2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringBootApplication.class, args);
+	}
+
+}
