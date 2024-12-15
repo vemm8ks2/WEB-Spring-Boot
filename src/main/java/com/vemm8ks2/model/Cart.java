7924371590c304283488cff93aaaeaa00f7e9653
vemm8ks2 <vemm8ks2@gmail.com>
2024-12-15 17:47:36 +0900

    STYLE: 일부 누락된 저장 사항

--- src/main/java/com/vemm8ks2/model/Cart.java
+++ src/main/java/com/vemm8ks2/model/Cart.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,7 +24,6 @@ public class Cart {
   @OneToOne
   private Users user;
 
-  @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true,
-      fetch = FetchType.EAGER)
+  @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<CartItems> cartItems = new ArrayList<>();
 }
