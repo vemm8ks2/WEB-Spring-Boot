c508776ea762cbcd3e6fefad8ef95a2ae9b06fa0
vemm8ks2 <vemm8ks2@gmail.com>
2024-12-15 22:21:44 +0900

    FEAT: 샘플 상품 SQL 추가

--- sql/상품 초기화.sql
+++ sql/상품 초기화.sql
@@ -3,3 +3,20 @@ VALUES (true, 'https://image.msscdn.net/thumbnails/images/goods_img/20220919/280
 INSERT INTO product_options VALUES (1, 260, 10, 1);
 INSERT INTO product_options VALUES (2, 270, 15, 1);
 INSERT INTO product_options VALUES (3, 280, 5, 1);
+
+INSERT INTO products(active, image_url, price, title, category_id)
+VALUES (true, 'https://image.msscdn.net/thumbnails/images/goods_img/20240422/4072173/4072173_17137531062050_big.jpg?w=1200', 103000, '810s 캠피 Black', '1-1-1');
+INSERT INTO product_options VALUES (4, 240, 30, 2);
+INSERT INTO product_options VALUES (5, 250, 30, 2);
+INSERT INTO product_options VALUES (6, 260, 5, 2);
+INSERT INTO product_options VALUES (7, 270, 15, 2);
+INSERT INTO product_options VALUES (8, 280, 5, 2);
+
+INSERT INTO products(active, image_url, price, title, category_id)
+VALUES (true, 'https://image.msscdn.net/thumbnails/images/goods_img/20210514/1951584/1951584_1_big.jpg?w=1200', 129000, '플레인 루버솔 스니커즈 화이트 남녀공용 TOBPR0106C34WH', '1-1-1');
+INSERT INTO product_options VALUES (9, 220, 20, 3);
+INSERT INTO product_options VALUES (10, 230, 20, 3);
+
+INSERT INTO products(active, image_url, price, title, category_id)
+VALUES (true, 'https://image.msscdn.net/thumbnails/images/goods_img/20231212/3752523/3752523_17023646599328_big.png?w=1200', 99000, '척 70 시즈널 컬러나이스 블루 A06529C', '1-1-1');
+INSERT INTO product_options VALUES (11, 280, 5, 4);
